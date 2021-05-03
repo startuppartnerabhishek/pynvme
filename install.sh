869d95c8f58b600b7ca24929ce4b48fcb617f228
Abhishek Mukherjee <abhishek@pensando.io>
2021-05-03 13:38:34 +0530

    add jq as a required package

--- install.sh
+++ install.sh
@@ -35,16 +35,16 @@
 
 if [ -s /etc/redhat-release ]; then
     # fedora, centos7
-    sudo yum install -y make redhat-rpm-config python3-devel python3-pip python3-tkinter
+    sudo yum install -y make redhat-rpm-config python3-devel python3-pip python3-tkinter cmake jq
 elif [ -f /etc/debian_version ]; then
     # ubuntu
-    sudo apt install -y python3-setuptools python3-dev python3-pip python3-tk
+    sudo apt install -y python3-setuptools python3-dev python3-pip python3-tk cmake jq
 elif [ -f /etc/SUSE-brand ]; then
     # SUSE
-    sudo zypper install -y python3-setuptools python3-devel python3-pip python3-tk
+    sudo zypper install -y python3-setuptools python3-devel python3-pip python3-tk cmake jq
 elif [ -f /etc/arch-release ]; then
     # ArchLinux
-    sudo pacman -S python3-setuptools python3-devel python3-pip tk
+    sudo pacman -S python3-setuptools python3-devel python3-pip tk cmake jq
 else
     echo "unknown system type."
     exit 1
