cb5d9f8c113b99f7cfa9e93ce91445ce6e0ea85b
Abhishek Mukherjee <abhishek@pensando.io>
2021-05-03 13:34:21 +0530

    beautify drv_sim log prints

--- src/driver/include/sim_common.h
+++ src/driver/include/sim_common.h
@@ -3,19 +3,21 @@
 
 #include <stdio.h>
 
-#define DRVSIM_LOG(_FMT_, ...) printf((_FMT_), ##__VA_ARGS__)
+#define __SHORTED_FILENAME__ (__builtin_strrchr(__FILE__, '/') ? __builtin_strrchr(__FILE__, '/') + 1 : __FILE__)
+
+#define DRVSIM_LOG(_FMT_, ...) printf("%s:%u %s(): "_FMT_, __SHORTED_FILENAME__, __LINE__, __FUNCTION__, ##__VA_ARGS__)
 
 #define DRVSIM_NOT_IMPLEMENTED(_FMT_, ...)  \
 do {                                        \
-    DRVSIM_LOG((_FMT_), ##__VA_ARGS__);     \
+    DRVSIM_LOG(_FMT_, ##__VA_ARGS__);     \
     fflush(stdout);                         \
     fflush(stderr);                         \
     assert(false);                          \
 } while (0)
 
-#define DRVSIM_TBD(_FMT_, ...)             \
-do {                                \
-    DRVSIM_LOG("TBD/TODO:" _FMT_, ##__VA_ARGS__);     \
+#define DRVSIM_TBD(_FMT_, ...)                          \
+do {                                                    \
+    DRVSIM_LOG("TBD/TODO:" _FMT_, ##__VA_ARGS__);       \
 } while (0)
 
 #define DRVSIM_RETCODE_SUCCESS 0
