05dac0da3232e1bf412fc9ec60ed99dea31124c7
Abhishek Mukherjee <abhishek@pensando.io>
2021-05-09 13:58:36 +0530

    add pretty logging for id-ctrlr response

--- src/driver/sim/sim_logging.c
+++ src/driver/sim/sim_logging.c
@@ -1,6 +1,18 @@
 #include "sim_common.h"
 #include "sim_types.h"
 
+#include "spdk/nvme_spec.h"
+
+#define LOG_TYPE_BANNER(__STRUCTNAME__, __CHECKPOINT__) DRVSIM_LOG("<<<<<<<<<<<<<<< %s (%s) >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n", __STRUCTNAME__, __CHECKPOINT__)
+
+#define LOG_FIELD_INT(__STRUCTPTR__, __FIELDNAME__, __CUSTOM_NAME__) DRVSIM_LOG_UNDECORATED_TO_FILE(stdout, ">> %s = 0x%x\n", __CUSTOM_NAME__ ? __CUSTOM_NAME__ : #__FIELDNAME__, (__STRUCTPTR__)->__FIELDNAME__)
+
+#define LOG_FIELD_HEX(__STRUCTPTR__, __FIELDNAME__, __CUSTOM_NAME__)                                                    \
+do {                                                                                                                    \
+    DRVSIM_LOG_UNDECORATED_TO_FILE(stdout, ">> %s >>>>>>>>>>\n", __CUSTOM_NAME__ ? __CUSTOM_NAME__ : #__FIELDNAME__);      \
+    sim_hex_dump(&((__STRUCTPTR__)->__FIELDNAME__), sizeof((__STRUCTPTR__)->__FIELDNAME__));                            \
+} while (0);
+
 ////module: log
 ///////////////////////////////
 
@@ -22,4 +34,79 @@ void log_cmd_dump_admin(ctrlr_t* ctrlr, size_t count)
   // TODO/TBD
   DRVSIM_TBD("not implemented\n");
   return;
+}
+
+void log_ctrlr_cmd(qpair_t *qp, sim_cmd_log_entry_t *cmd_log_entry)
+{
+    bool is_adminq = SIM_IS_ADMINQ(qp);
+    const char *cmd_name_as_string = cmd_name(cmd_log_entry->cmd.opc, is_adminq ? 0 : 1);
+
+    DRVSIM_LOG("ctrlr %p is sending command on %s qp %p\n",
+        qp->parent_controller, is_adminq ? "ADMIN" : "IO", qp);
+
+    DRVSIM_LOG("Command opc %s (%u), fuse %u, cid 0x%x\n",
+        cmd_name_as_string, cmd_log_entry->cmd.opc,
+        cmd_log_entry->cmd.fuse, cmd_log_entry->cmd.cid);
+
+    sim_hex_dump(&cmd_log_entry->cmd, sizeof(cmd_log_entry->cmd));
+ 
+    return;
+}
+
+void log_ctrlr_completion(qpair_t *qp, sim_cmd_log_entry_t *cmd_log_entry)
+{
+    bool is_adminq = SIM_IS_ADMINQ(qp);
+    const char *cmd_name_as_string = cmd_name(cmd_log_entry->cmd.opc, is_adminq ? 0 : 1);
+
+    DRVSIM_LOG("ctrlr %p got completion on %s qp %p\n",
+        qp->parent_controller, is_adminq ? "ADMIN" : "IO", qp);
+
+    DRVSIM_LOG("|||| Command was opc %s (%u), fuse %u, cid 0x%x --------->\n",
+        cmd_name_as_string, cmd_log_entry->cmd.opc,
+        cmd_log_entry->cmd.fuse, cmd_log_entry->cmd.cid);
+
+    DRVSIM_LOG("|||| Response status-code %u, phase-tag %u, code-type %u, more %u, dnr %u\n",
+        cmd_log_entry->cpl.status.sc, cmd_log_entry->cpl.status.p, cmd_log_entry->cpl.status.sct,
+        cmd_log_entry->cpl.status.m, cmd_log_entry->cpl.status.dnr);
+
+    DRVSIM_LOG("|||| cdw0 0x%x, sq-head %u, sq-id %u, cid 0x%x\n",
+        cmd_log_entry->cpl.cdw0, cmd_log_entry->cpl.sqhd,
+        cmd_log_entry->cpl.sqid, cmd_log_entry->cpl.cid);
+
+    sim_hex_dump(&cmd_log_entry->cpl, sizeof(cmd_log_entry->cpl));
+
+    return;
+}
+
+void log_ctrlr_completion_buf_id_controller(sim_cmd_log_entry_t *cmd_log_entry)
+{
+    struct spdk_nvme_ctrlr_data *resp = cmd_log_entry->response_buf;
+
+    LOG_TYPE_BANNER("Idenitfy Controller Response", "START");
+
+    LOG_FIELD_INT(resp, vid, "PCI Vendor ID");
+    LOG_FIELD_INT(resp, ssvid, "PCI subsystem vendor id");
+    LOG_FIELD_HEX(resp, sn, "Serial Number");
+    LOG_FIELD_HEX(resp, mn, "Model Number");
+    LOG_FIELD_HEX(resp, fr, "Firmware Revision");
+    LOG_FIELD_HEX(resp, ieee, "IEEE OUI Id");
+    LOG_FIELD_INT(resp, mdts, "Max data transfer size");
+    LOG_FIELD_INT(resp, cntlid, "Controller ID");
+    LOG_FIELD_HEX(resp, ver, "NVME version");
+    LOG_FIELD_INT(resp, aerl, "Async Event Request limit");
+    LOG_FIELD_HEX(resp, lpa, "Log page attributes");
+    LOG_FIELD_INT(resp, npss, "Number of power states supported");
+    LOG_FIELD_INT(resp, hmpre, "Host Memory preferred size");
+    LOG_FIELD_INT(resp, hmmin, "Host Memory min size");
+    LOG_FIELD_HEX(resp, tnvmcap, "Total NVM capacity");
+    LOG_FIELD_HEX(resp, unvmcap, "Unallocated NVM capacity");
+    LOG_FIELD_HEX(resp, sqes, "Submission queue entry-size min-max");
+    LOG_FIELD_HEX(resp, cqes, "Completion queue entry-size min-max");
+    LOG_FIELD_INT(resp, maxcmd, NULL);
+    LOG_FIELD_INT(resp, nn, "Number of namespaces");
+    LOG_FIELD_HEX(resp, subnqn, "Subsystem NQN");
+
+    LOG_TYPE_BANNER("Idenitfy Controller Response", "END");
+
+    return;
 }
