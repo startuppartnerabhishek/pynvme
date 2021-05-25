7b9cb4f4c1c6f384b1ef1f96cd82b2fce6860d21
Abhishek Mukherjee <abhishek@pensando.io>
2021-05-25 13:25:53 +0530

    add support for byte-fields, reduce default framework logging verbosity

--- scripts/pensando/py-utils/validator.py
+++ scripts/pensando/py-utils/validator.py
@@ -102,6 +102,38 @@ class FieldInt(Field):
             logging.error(specValue)
             assert False
 
+class FieldBytes(Field):
+    def __init__(self, name, nodeInCfg, nodeInSpec):
+        super().__init__(name, nodeInCfg, nodeInSpec)
+        self.type = "bytes"
+
+    def compare(self, rootObj, compareWith):
+        specValue = self.getSpecField(compareWith)
+        fullPathAsList = rootObj.split('.') # rootObj can itself be a path, e.g. nvme0.nn0
+        fullPathAsList.extend(self.pathToCfgField)
+
+        cfgValue = CfgStore.getConfigDeep(fullPathAsList)
+
+        if None == cfgValue:
+            # value not found in store, don't fail the test
+            warnings.warn("(Bytes) NOT FOUND in store " + rootObj + "." + self.structNodeInCfg)
+            logging.warning("(Bytes) %s should have been in Cfg Store at %s.%s", self.name, rootObj, self.pathToCfgField)
+            logging.warning(CfgStore.getAllConfig())
+            return
+
+        cfgAsBytes = bytes.fromhex(cfgValue)
+        specValueAsBytes = bytes(specValue)
+
+        if cfgAsBytes != specValueAsBytes:
+            logging.error("Object %s, field %s, path %s, spec-field %s type INT", rootObj, self.name, self.structNodeInCfg, self.structNodeInSpec)
+            logging.error("Expected (raw)")
+            logging.error(cfgAsBytes)
+            logging.error(cfgValue)
+            logging.error("Got (raw)")
+            logging.error(specValueAsBytes)
+            logging.error(specValue)
+            assert False
+
 class StructValidator:
     def __init__(self, fieldList, specStructName):
         self.allFields = fieldList
@@ -110,7 +142,7 @@ class StructValidator:
     def compare(self, rootObj, compareWithRawBuf):
         structDict = Parser.struct_parse_as_dict(compareWithRawBuf[0:Parser.struct_size(self.specStruct)], self.specStruct)
 
-        logging.info("%s validator with field list [%s]", self.specStruct, self.allFields)
+        logging.debug("%s validator with field list [%s]", self.specStruct, self.allFields)
 
         for field in self.allFields:
             field.compare(rootObj, structDict)
@@ -123,6 +155,7 @@ gControllerValidator = StructValidator([
     FieldInt("Vendor Id", "vid", "vid"),
     FieldString("Serial Number", "serial", "sn"),
     FieldInt("Version", "version", "ver.raw"),
+    FieldBytes("IEEE OUI Id", "ieee", "ieee")
 ], "spdk_nvme_ctrlr_data")
 
 # external API
