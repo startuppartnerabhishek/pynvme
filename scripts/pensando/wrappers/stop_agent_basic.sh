46757d9517ce563e20a32b25cd865ed97e0959d9
Abhishek Mukherjee <abhishek@pensando.io>
2021-05-31 14:43:01 +0530

    minor prints to stop-agent script

--- scripts/pensando/wrappers/stop_agent_basic.sh
+++ scripts/pensando/wrappers/stop_agent_basic.sh
@@ -2,5 +2,9 @@
 
 AGENT_NAME_PATTERN="nvme_agent"
 
+echo "pkill-ing agent pattern ${AGENT_NAME_PATTERN}"
+
 # unceremoniously kill the agent
 pkill -9 ${AGENT_NAME_PATTERN}
+
+echo "Older agent instance(s) killed"
